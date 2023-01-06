6ca16cdc9598491ce265dfba7d31f6f723af671f
Robert Bendun <rob.bendun@gmail.com>
2023-01-06 16:53:58 +0100

    Integrated Ableton/link library

--- scripts/macos.mk
+++ scripts/macos.mk
@@ -1,6 +1,6 @@
 CC=clang
 CXX=clang++
-CPPFLAGS:=$(CPPFLAGS) -D __MACOSX_CORE__
+CPPFLAGS:=$(CPPFLAGS) -D __MACOSX_CORE__ -D LINK_PLATFORM_MACOSX
 LDLIBS:=-framework CoreMIDI -framework CoreAudio -framework CoreFoundation $(LDLIBS)
 Release_Obj=$(addprefix bin/,$(Obj))
 Bestline=bin/$(os)/bestline.o
