1632ef74af519bb4cd3d877f5f196378e3ff962b
Robert Bendun <robert@bendun.cc>
2023-03-05 01:31:32 +0100

    Remove bestline from project

--- scripts/linux.mk
+++ scripts/linux.mk
@@ -2,5 +2,4 @@ CC=gcc
 CXX=g++
 CPPFLAGS:=$(CPPFLAGS) -D __LINUX_ALSA__ -D LINK_PLATFORM_LINUX
 LDLIBS:=-lasound $(LDLIBS) -static-libgcc -static-libstdc++
-Bestline=bin/$(os)/bestline.o
 Target=musique
--- scripts/macos.mk
+++ scripts/macos.mk
@@ -3,5 +3,4 @@ CXX=clang++
 CPPFLAGS:=$(CPPFLAGS) -D __MACOSX_CORE__ -D LINK_PLATFORM_MACOSX
 LDLIBS:=-framework CoreMIDI -framework CoreAudio -framework CoreFoundation $(LDLIBS)
 Release_Obj=$(addprefix bin/,$(Obj))
-Bestline=bin/$(os)/bestline.o
 Target=musique
